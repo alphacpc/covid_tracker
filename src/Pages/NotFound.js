17c352f318f8b5d855a288e11afe2e4a9c2ccf45
alphacpc <alphacpc@gmail.com>
2021-08-24 21:09:01 +0000

    final page 404

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import mySVG from './../assets/images/mySVG5.svg'
+
 
 const NotFound = () => {
     return (
         <div>
             <div className="WordstatsContainer">
-                <img width="44%" src={mySVG} style={{marginTop:'40vh'}}/>
+                <img width="44%" src={mySVG} style={{marginTop:'30vh'}}/>
             </div>
-            <h1>Oups !</h1>
-            <p>Page introuvable</p>
+            <div className="MessageNotFound">
+                <h1>Oups !</h1>
+                <p>La page que vous recherchez semble introuvable.</p>
+                <Link to="/" className="LinkBackHome">Retour à la page d'accueil</Link>
+            </div>
         </div>
     )
 }
